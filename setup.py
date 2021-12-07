da41a92b4c34a1125bf034a0913fdff41a765619
Sneeky-Man <67141208+Sneeky-Man@users.noreply.github.com>
2021-12-07 23:36:21 +0000

    Attemping to set up a config file v4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='The_Project',
-    version='',
+    version='0.1',
     packages=['the_project', 'the_project.entities', 'the_project.special_scripts'],
     url='https://github.com/Sneeky-Man/The_Project',
     license='MIT',
