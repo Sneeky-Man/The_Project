28c5c1ea199a89fc51f935cf0ca28c5dec694699
Sneeky-Man <67141208+Sneeky-Man@users.noreply.github.com>
2022-02-19 17:03:45 +0000

    Bullet is now a subclass of arcade.Sprite, instead of Entity

--- the_project/entities/bullet.py
+++ the_project/entities/bullet.py
@@ -1,37 +1,42 @@
 import arcade
 
-
 from the_project.entities.entity import Entity
 from the_project.constants import *
 import math
 
 
-class Bullet(Entity):
-    def __init__(self, name: str, tier: int, path: str, speed: float, parent: object):
+class Bullet(arcade.Sprite):
+    def __init__(self, path: str, speed: float, damage: int, parent: object):
         """
         This is the class that all bullets will be.
 
-        :param name: The name of the entity
-        :param tier: The tier of the entity
         :param path: Path to the texture
         :param speed: Speed of the bullet
+        :param damage: Damage of the Bullet
         :param parent: The entity that shot the bullet
         """
-        super().__init__(name=name, tier=tier, team=parent.get_team(), path=path, x=parent.center_x, y=parent.center_y)
+        super().__init__()
 
+        self.__path_to_texture = path
         self.__speed = speed
+        self.__damage = damage
         self.__parent = parent
         self.scale = 0.5
 
+        self.texture = arcade.load_texture(path)
+        self.center_x = self.__parent.center_x
+        self.center_y = self.__parent.center_y
+        self.__team = self.__parent.get_team()
+
         # Math stolen from asteroid_smasher.py
-        self.angle = parent.angle
+        self.angle = self.__parent.angle
         self.change_y = \
             math.cos(math.radians(self.angle)) * self.__speed
         self.change_x = \
             -math.sin(math.radians(self.angle)) \
             * self.__speed
 
-    def __str__(self):
+    def __repr__(self):
         return f"Bullet. Parent {self.__parent}"
 
     def check_for_collision(self, window):
@@ -52,7 +57,7 @@ class Bullet(Entity):
                                                                     ])
         for collision in collision_list:
             if isinstance(collision, Entity):
-                if not self.same_team(collision):
+                if not collision.same_team(self):
                     # Have to do it here, or it throws a circular import error
                     # from the_project.entities.building import Building
                     # if isinstance(collision, Building):
@@ -62,7 +67,39 @@ class Bullet(Entity):
                     self.kill()
 
     def get_parent(self):
+        """
+        :return: The parent of the Bullet.
+        :rtype: object
+        """
         return self.__parent
+
+    def get_damage(self):
+        """
+        :return: The damage of the bullet
+        :rtype: int
+        """
+        return self.__damage
+
+    def get_speed(self):
+        """
+        :return: The speed of the bullet
+        :rtype: float
+        """
+        return self.__speed
+
+    def get_team(self):
+        """
+        :return: The team of the entity
+        :rtype: str
+        """
+        return self.__team
+
+    def get_path(self):
+        """
+        :return: The path of the entity
+        :rtype: str
+        """
+        return self.__path_to_texture
 
     def update(self, window):
         """
@@ -70,8 +107,8 @@ class Bullet(Entity):
 
         :param window: The Game_Window
         """
-        # Code stolen from asteroid_smasher.py
         super().update()
+        # Code stolen from asteroid_smasher.py
         self.check_for_collision(window)
         self.angle = math.degrees(math.atan2(self.change_y, self.change_x)) - 90
 
